e57d96f4183c075ce66630045dba09d88f1d74a0
sadovnikov.russia <sadovnikov.russia@gmail.com>
2018-07-20 11:26:38 +0300

    Добавлены два файла Hello.java

---
+++ src/tech/sadovnikov/Hello1.java
@@ -0,0 +1,7 @@
+package tech.sadovnikov;
+
+public class Hello1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, world 1");
+    }
+}
---
+++ src/tech/sadovnikov/Hello2.java
@@ -0,0 +1,4 @@
+package tech.sadovnikov;
+
+public class Hello2 {
+}
